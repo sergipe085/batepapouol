807b7317971bab971294de89d2d3f0ef9669ccad
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-09-06 13:20:41 -0300

    connecting user to room

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,7 +1,31 @@
 import { io } from "./http";
 
+interface IUserRoom {
+    socket_id: string;
+    username: string;
+    room: string;
+}
+
+const users: IUserRoom[] = [];
+
 io.on("connection", (socket) => {
     socket.on("select_room", (data) => {
-        console.log(data);
+        socket.join(data.room);
+
+        const userInRoom = users.find(
+            (user) => user.username === data.username && user.room === data.room
+        );
+
+        if (userInRoom) {
+            userInRoom.socket_id = socket.id;
+        } else {
+            users.push({
+                socket_id: socket.id,
+                username: data.username,
+                room: data.room,
+            });
+        }
+
+        console.log(users);
     });
 });
